fb23450371be1a10f77e006826152ea10827ac71
Kuwar123 <55982922+Kuwar123@users.noreply.github.com>
2021-05-04 14:39:27 +0530

    Add files via upload

---
+++ main/java/com/example/googlemapdemo/MainActivity.java
@@ -0,0 +1,100 @@
+package com.example.googlemapdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import static com.google.android.gms.maps.SupportMapFragment.*;
+
+public class MainActivity extends AppCompatActivity {
+    //Initialize variable
+    SupportMapFragment supportMapFragment;
+    FusedLocationProviderClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Assign Variable
+        supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.google_map);
+
+        //Initialize fused location
+        client = LocationServices.getFusedLocationProviderClient(this);
+
+        //Check Permission
+        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            //WHEN PERMISSION GRANTED CALL METHOD
+            getCurrentLocation();
+        } else {
+            //When permission denied
+            //Request Permission
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+        }
+    }
+
+    private void getCurrentLocation() {
+        //Initialize task location
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Task<Location> task = client.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                //When success
+                if(location!=null){
+                    //sync map
+                    SupportMapFragment.getMapAsync(new OnMapReadyCallback() {
+                        @Override
+                        public void onMapReady(GoogleMap googleMap) {
+                            LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());
+
+                            //CREATE MARKER OPTIONS
+                            MarkerOptions options = new MarkerOptions().position(latLng).title("I am there");
+                            //Zoom Map
+                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,10));
+
+                            //ADD MARKERR ON MAP
+                            googleMap.addMarker(options);
+                        }
+                    });
+                }
+
+            }
+        });
+
+    }
+
+    @Override
+    public void overridePendingTransition(int enterAnim, int exitAnim) {
+        if(requestCode = 44 ){
+            if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                //When Permission granted  call method
+                getCurrentLocation();
+            }
+        }
+    }
+}
